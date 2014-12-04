2793d9d2cd3f11e141f135031264f0a4f29115b1
Alexey Salnikov <alexey.salnikov@gmail.com>
2014-12-04 15:48:45 +0300

    The modes "create" and "delete" has been add to pseudo_users_and_groups_operations.py

--- scripts/pseudo_users_and_groups_operations.py
+++ scripts/pseudo_users_and_groups_operations.py
@@ -5,6 +5,100 @@ import argparse
 import os
 import sys
 
+
+def create_users_and_groups(args):
+    if os.path.exists(args.homes_prefix):
+        if not os.path.isdir(args.homes_prefix):
+            print "path '%s' is not a directory" % args.homes_prefix
+            return 3
+    else:
+        try:
+            print "make path to homes: '%s'" % args.homes_prefix
+            os.makedirs(args.homes_prefix,0755)
+        except OSError, e:
+            print e
+            return 3
+
+    user_group_map=dict()
+
+    file_descr=open(args.prefix+"user_in_groups_map","r")
+    for  line in file_descr:
+        tupl=line.split(':')
+        user=tupl[0].strip()
+        groups_line=tupl[1].strip()
+        groups_tupl=groups_line.split(',')
+        groups=list()
+        for group in groups_tupl:
+            groups.append(group.strip(' \t\n\r'))
+        user_group_map[user]=groups
+    file_descr.close()
+
+    file_descr=open(args.prefix+"groups_map","r")
+    for line in file_descr:
+        tupl=line.split(':')
+        group=tupl[0].strip()
+
+        command_line  = "groupadd --force  '%s'" % group
+        
+        print "create group: '%s'" % group
+        if os.system(command_line):
+            return 1
+        #print command_line
+    file_descr.close()
+        
+
+    file_descr=open(args.prefix+"users_map","r")
+    for line in file_descr:
+        tupl=line.split(':')
+        user=tupl[0].strip()
+
+        command_line="useradd "
+        command_line+="--create-home --home \'%s/%s\' " % (args.homes_prefix, user)
+        command_line+="--gid '%s' " % user_group_map[user][0]
+        groups_line=""
+        for i in xrange (1, len(user_group_map[user])):
+            groups_line+="%s," % user_group_map[user][i]
+        groups_line=groups_line.strip("\t\r '")
+        if groups_line != "":
+            command_line+="--groups '%s'"
+        command_line+=" '%s'" % user
+
+        print "create user: '%s'" % user
+        if os.system(command_line):
+            return 1
+        #print command_line
+    file_descr.close()
+
+def delete_users_and_groups(args):
+    file_descr=open(args.prefix+"users_map","r")
+    for line in file_descr:
+        tupl=line.split(':')
+        user=tupl[0].strip()
+
+        command_line="userdel '%s'" % user
+
+        print "delete user: '%s'" % user
+        if os.system(command_line):
+            print "            warning for user '%s'" % user
+        #print command_line
+    file_descr.close()
+
+    file_descr=open(args.prefix+"groups_map","r")
+    for line in file_descr:
+        tupl=line.split(':')
+        group=tupl[0].strip()
+
+        command_line  = "groupdel '%s'" % group
+
+        print "delete group: '%s'" % group
+        if os.system(command_line):
+            print "             warning for group '%s'" % group
+        #print command_line
+    file_descr.close()
+
+    return 0
+
+
 def main(argv=None):
     """
         То, с чего начинается программа
@@ -34,6 +128,25 @@ def main(argv=None):
     )
 
     parser.add_argument(
+            '--mode',
+            dest='mode',
+            required=False,
+            choices=["create","delete"],
+            default="create",
+            help=\
+                    """
+                    определяет режим в котором всё работает:
+
+                        create -- создаются пользователи и группы,
+                                  а так же домашние каталоги 
+                                  пользователей.
+
+                        delete -- удаляются пользователи и группы,
+                                  каталоги пользователей остаются неизменными.
+                    """
+    )
+
+    parser.add_argument(
             '--homes-prefix',
             dest='homes_prefix',
             required=False,
@@ -54,53 +167,15 @@ def main(argv=None):
               """
         return 2
     
-    user_group_map=dict()
+    if args.mode == "create":
+        return create_users_and_groups(args)
 
-    file_descr=open(args.prefix+"user_in_groups_map","r")
-    for  line in file_descr:
-        tupl=line.split(':')
-        user=tupl[0].strip()
-        groups_line=tupl[1].strip()
-        groups_tupl=groups_line.split(',')
-        groups=list()
-        for group in groups_tupl:
-            groups.append(group.strip(' \t\n\r'))
-        user_group_map[user]=groups
-    file_descr.close()
+    if args.mode == "delete":
+        return delete_users_and_groups(args)
 
-    file_descr=open(args.prefix+"groups_map","r")
-    for line in file_descr:
-        tupl=line.split(':')
-        group=tupl[0].strip()
-
-        command_line  = "groupadd --force  '%s'" % group
-        if os.system(command_line):
-            return 1
-        #print command_line
-    file_descr.close()
-        
-
-    file_descr=open(args.prefix+"users_map","r")
-    for line in file_descr:
-        tupl=line.split(':')
-        user=tupl[0].strip()
-
-        command_line="useradd "
-        command_line+="--create-home --home \'%s/%s\' " % (args.homes_prefix, user)
-        command_line+="--gid '%s' " % user_group_map[user][0]
-        groups_line=""
-        for i in xrange (1, len(user_group_map[user])):
-            groups_line+="%s," % user_group_map[user][i]
-        groups_line=groups_line.strip("\t\r '")
-        if groups_line != "":
-            command_line+="--groups '%s'"
-        command_line+=" %s" % user
-        if os.system(command_line):
-            return 1
-        #print command_line
-    file_descr.close()
-
-    return 0
+   
+   
+    return 100
 
 
 if __name__ == "__main__":
