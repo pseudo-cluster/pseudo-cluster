98917661f7f979bac1c5b16c89542b4490e1aaf3
Alexey Salnikov <alexey.salnikov@gmail.com>
2014-12-11 19:10:13 +0300

    metric

--- scripts/pseudo_cluster/metrics/metric_average_queue_time.py
+++ scripts/pseudo_cluster/metrics/metric_average_queue_time.py
@@ -1,11 +1,39 @@
 # -*- coding: utf-8 -*-
 
+metric_short_description="вычисляет среднее время ожидания в очереди."
+
 metric_description=\
 """
-вычисляет среднее время ожидания в очереди.
+Среднее время ожидания в очереди для задач пользователя. 
+Выражает степень недовольства пользователей вычислительным 
+кластером, поскольку никому не хочется находиться в очереди долго.
+
+требует параметров:
+    count_mode - возможные значения: (user, day, total)
 """
 
-def count_values(task_list,count_parameters):
+def class Metric_counter(object):
+    """
+        Класс задающий метрику
+    """
+
+    def __init__(self,tasks_list,parameters):
+        """
+        Собственно конструирование объекта
+        """
+        self.tasks_list=tasks_list
+        self.parameters=dict()
+        if parameters != "":
+            for item in other_string.split(','):
+                pair=item.strip().split('=')
+                self.parameters[pair[0]]=pair[1].strip("'\"")
+
+    def __str__(self):
+        s="package %s: Metric_counter: " % __name__
+        s+="tasks_list=%s, " % str(self.tasks_list)
+        s+="parameters=%s " % str(self.parameters)
+
+    def count_values(self,compression):
     """
     Подсчитать и выдать число,
     словарь значений, и т.п.
