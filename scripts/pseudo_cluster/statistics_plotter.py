292b5a0ad7b52d6024bae76a4709e827e3e7b2c9
Alexey Salnikov <alexey.salnikov@gmail.com>
2015-01-27 16:55:42 +0300

    Plot more attractive.

--- scripts/pseudo_cluster/statistics_plotter.py
+++ scripts/pseudo_cluster/statistics_plotter.py
@@ -26,16 +26,24 @@ class Plotter(object):
 
         if self.draw_type == 'chart':
             plt=pygal.Bar()
-            #plt.x_labels(self.data.keys())
-            plt.add(self.labels[1],self.data.values())            
+            plt.x_labels=map(str,self.data.keys())
+            plt.add("data",self.data.values())
+            #
+            # May be in graduses
+            #
+            plt.x_label_rotation=90 
 
         if self.draw_type == 'plot':
-            plt=pygal.XY()
-            data_array=list()
-            for k,v  in self.data.items():
-                data_array.append((float(k),float(v)))
-            plt.add(self.labels[1], data_array )
+            plt=pygal.Line()
+            plt.x_labels=map(str,self.data.keys())
+            plt.add("data",self.data.values())
 
-        plt.title=self.title
+        plt.show_legend=False
+        plt.label_font_size=11
+        plt.width=1024
+        plt.height=768
+        plt.title=unicode(self.title)
+        plt.x_title=unicode(self.labels[0])
+        plt.y_title=unicode(self.labels[1])
         plt.render_to_file(file_name)
 
